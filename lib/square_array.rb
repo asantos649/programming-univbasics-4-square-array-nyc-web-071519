e86be798bb012cae2258c0f6f3998570e364442c
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 18:41:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count=0
   new_array = []
   while array[count]
-    new_array.pop(array[count] ** 2)
+    new_array.push(array[count] ** 2)
     count += 1 
   end
   return new_array
